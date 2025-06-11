7d5d54ec921606ec4bb4108ae499f680b2bf4556
JoaoCarlosOT <joaocarlosdev403@gmail.com>
2025-06-11 16:36:28 -0300

    carrossel de avaliações adicionado

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ function AbrirEFecharMenu() {
     }
 }
 
+  $(document).ready(function(){
+    $('.car-carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: false,           // Desativa as setas
+      dots: true,           // Ativa as bolinhas
+      responsive: {
+        0: {
+          items: 1          // 1 card por vez no mobile
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3          // 3 cards lado a lado em telas grandes
+        }
+      }
+    });
+  });
+
+
 // window.addEventListener('scroll', function() {
 //     var button = document.getElementById('butao-main');
 //     var scrollPosition = window.scrollY;
